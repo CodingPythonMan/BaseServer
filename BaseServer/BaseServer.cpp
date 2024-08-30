146b5e85fbe487fc887ebd541bb15045c7957c8a
Kim Ju Young <robolab1902@naver.com>
2024-08-30 15:04:19 +0900

    BaseServer 랑 NetworkManager 가 따로 구분되어있다. 주말에 좀 더 구조를 구체화해보자.

--- BaseServer/BaseServer.cpp
+++ BaseServer/BaseServer.cpp
@@ -1,5 +1,8 @@
 #include "BaseServer.h"
 #include "ConfigParser.h"
+#include "Listener.h"
+
+#include <memory>
 
 BaseServer::BaseServer()
 {
@@ -16,6 +19,13 @@ bool BaseServer::Initialize()
 {
 	ConfigParser::GetInstance()->LoadConfig(L"GameConfig.json");
 
+	// 다양한 Manager 들에 대한 코드가 들어갈 수 있다.
+	// 그런데... 시작은 어떤 코드가 들어가야할까?
+
+	// 우선 Make Shared 하는 이유에 대해선 좀 더 조사가 필요하다.
+	// 왜 Make Shared 하게 되는 걸까?
+	std::shared_ptr<Listener> listener = std::make_shared<Listener>();
+
 
 
 	return true;
@@ -23,7 +33,9 @@ bool BaseServer::Initialize()
 
 bool BaseServer::Run()
 {
-
+	// 이 때부터 스레드를 파서 로직 스레드, IOCP 스레드를 나눈다.
+	// 해당 부분을 나눠서 짤 수 있는걸까?
+	
 
 	return false;
 }
---
+++ ServerCore/Listener.h
@@ -0,0 +1,10 @@
+#pragma once
+// ServerCore 에 들어가야 알맞은 값이 된다.
+class Listener
+{
+public:
+	Listener();
+	virtual ~Listener();
+
+
+};
